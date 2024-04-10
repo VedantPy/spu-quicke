2f01494ed37ca0b494de3199ea062db3d3f31de8
Vedant Pandey <vedantpandey434@gmail.com>
2024-04-10 14:14:05 +0530

    Update Card component to include command prop

--- src/App.jsx
+++ src/App.jsx
@@ -58,44 +58,9 @@ function App() {
       image:
         "https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg",
     },
+
     {
       id: 7,
-      name: "TensorFlow",
-      url: "https://www.tensorflow.org/",
-      description:
-        "TensorFlow is an open-source library for numerical computation and large-scale machine learning.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/2/2d/Tensorflow_logo.svg",
-    },
-    {
-      id: 8,
-      name: "PyTorch",
-      url: "https://pytorch.org/",
-      description:
-        "PyTorch is an open-source machine learning library based on the Torch library.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/96/Pytorch_logo.png",
-    },
-    {
-      id: 9,
-      name: "Scikit-learn",
-      url: "https://scikit-learn.org/",
-      description:
-        "Scikit-learn is a free software machine learning library for the Python programming language.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg",
-    },
-    {
-      id: 10,
-      name: "Keras",
-      url: "https://keras.io/",
-      description:
-        "Keras is an open-source deep learning library written in Python.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/a/ae/Keras_logo.svg",
-    },
-    {
-      id: 11,
       name: "Fast.ai",
       url: "https://www.fast.ai/",
       description:
@@ -104,7 +69,7 @@ function App() {
         "https://thumb.tildacdn.com/tild3263-3763-4737-a163-623437306266/-/resize/280x/-/format/webp/fastai.png",
     },
     {
-      id: 12,
+      id: 8,
       name: "Hugging Face",
       url: "https://huggingface.co/",
       description:
@@ -113,52 +78,7 @@ function App() {
         "https://huggingface.co/datasets/huggingface/brand-assets/resolve/main/hf-logo-with-title.png",
     },
     {
-      id: 13,
-      name: "OpenCV",
-      url: "https://opencv.org/",
-      description:
-        "OpenCV is an open-source computer vision and machine learning software library.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/3/32/OpenCV_Logo_with_text_svg_version.svg",
-    },
-    {
-      id: 14,
-      name: "Pandas",
-      url: "https://pandas.pydata.org/",
-      description:
-        "pandas is a fast, powerful, flexible, and easy-to-use open-source data analysis and data manipulation library built on top of the Python programming language.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/e/ed/Pandas_logo.svg",
-    },
-    {
-      id: 15,
-      name: "NumPy",
-      url: "https://numpy.org/",
-      description:
-        "NumPy is an open-source numerical computing library for the Python programming language.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/3/31/NumPy_logo_2020.svg",
-    },
-    {
-      id: 16,
-      name: "Matplotlib",
-      url: "https://matplotlib.org/",
-      description:
-        "Matplotlib is a plotting library for the Python programming language and its numerical mathematics extension NumPy.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/0/01/Created_with_Matplotlib-logo.svg",
-    },
-    {
-      id: 17,
-      name: "Seaborn",
-      url: "https://seaborn.pydata.org/",
-      description:
-        "Seaborn is a Python data visualization library based on matplotlib.",
-      image:
-        "https://user-images.githubusercontent.com/315810/92255284-156f1180-eea0-11ea-9d2d-be8262670e8c.png",
-    },
-    {
-      id: 18,
+      id: 9,
       name: "Plotly",
       url: "https://plotly.com/",
       description:
@@ -167,7 +87,7 @@ function App() {
         "https://upload.wikimedia.org/wikipedia/commons/8/8a/Plotly-logo.png",
     },
     {
-      id: 19,
+      id: 10,
       name: "Bokeh",
       url: "https://bokeh.org/",
       description:
@@ -175,7 +95,7 @@ function App() {
       image: "https://static.bokeh.org/logos/logotype.svg",
     },
     {
-      id: 20,
+      id: 11,
       name: "Streamlit",
       url: "https://streamlit.io/",
       description:
@@ -183,27 +103,232 @@ function App() {
       image:
         "https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.svg",
     },
+    {
+      id: 12,
+      name: "LightingAI",
+      url: "https://lightning.ai/",
+      description:
+        "LightningAI is a platform that enables you to create and deploy machine learning models.",
+      image:
+        "https://avatars.githubusercontent.com/u/58386951?s=200&v=4",
+    }
+  ];
+
+  const pythonPackages = [
+    {
+      id: 1,
+      name: "TensorFlow",
+      url: "https://www.tensorflow.org/",
+      description:
+        "TensorFlow is an open-source library for numerical computation and large-scale machine learning.",
+
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/2/2d/Tensorflow_logo.svg",
+      command: "pip install tensorflow",
+    },
+    {
+      id: 2,
+      name: "PyTorch",
+      url: "https://pytorch.org/",
+      description:
+        "PyTorch is an open-source machine learning library based on the Torch library.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/9/96/Pytorch_logo.png",
+      command: "pip install torch",
+    },
+    {
+      id: 3,
+      name: "Scikit-learn",
+      url: "https://scikit-learn.org/",
+      description:
+        "Scikit-learn is a free software machine learning library for the Python programming language.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg",
+      command: "pip install scikit-learn",
+    },
+    {
+      id: 4,
+      name: "Keras",
+      url: "https://keras.io/",
+      description:
+        "Keras is an open-source deep learning library written in Python.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/a/ae/Keras_logo.svg",
+      command: "pip install keras",
+    },
+    {
+      id: 5,
+      name: "OpenCV",
+      url: "https://opencv.org/",
+      description:
+        "OpenCV is an open-source computer vision and machine learning software library.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/3/32/OpenCV_Logo_with_text_svg_version.svg",
+      command: "pip install opencv-python",
+    },
+    {
+      id: 6,
+      name: "Pandas",
+      url: "https://pandas.pydata.org/",
+      description:
+        "pandas is a fast, powerful, flexible, and easy-to-use open-source data analysis and data manipulation library built on top of the Python programming language.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/e/ed/Pandas_logo.svg",
+      command: "pip install pandas",
+    },
+    {
+      id: 7,
+      name: "NumPy",
+      url: "https://numpy.org/",
+      description:
+        "NumPy is an open-source numerical computing library for the Python programming language.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/3/31/NumPy_logo_2020.svg",
+      command: "pip install numpy",
+    },
+    {
+      id: 8,
+      name: "Matplotlib",
+      url: "https://matplotlib.org/",
+      description:
+        "Matplotlib is a plotting library for the Python programming language and its numerical mathematics extension NumPy.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/0/01/Created_with_Matplotlib-logo.svg",
+      command: "pip install matplotlib",
+    },
+    {
+      id: 9,
+      name: "Seaborn",
+      url: "https://seaborn.pydata.org/",
+      description:
+        "Seaborn is a Python data visualization library based on matplotlib.",
+      image:
+        "https://user-images.githubusercontent.com/315810/92255284-156f1180-eea0-11ea-9d2d-be8262670e8c.png",
+      command: "pip install seaborn",
+    },
+    {
+      id: 10,
+      name: "NLTK",
+      url: "https://www.nltk.org/",
+      description:
+        "NLTK is a leading platform for building Python programs to work with human language data.",
+      image:
+        "https://miro.medium.com/v2/resize:fit:592/1*YM2HXc7f4v02pZBEO8h-qw.png",
+      command: "pip install nltk",
+    },
+    {
+      id: 11,
+      name: "Gensim",
+      url: "https://radimrehurek.com/gensim/",
+      description:
+        "Gensim is a Python library for topic modelling, document indexing, and similarity retrieval with large corpora.",
+      image:
+        "https://radimrehurek.com/gensim/_images/gensim_logo_positive_complete_tb.png",
+      command: "pip install gensim",
+    },
+    {
+      id: 12,
+      name: "Spacy",
+      url: "https://spacy.io/",
+      description:
+        "spaCy is a free, open-source library for advanced Natural Language Processing (NLP) in Python.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/SpaCy_logo.svg/2560px-SpaCy_logo.svg.png",
+      command: "pip install spacy",
+    },
+    {
+      id: 13,
+      name: "Flask",
+      url: "https://flask.palletsprojects.com/en/2.0.x/",
+      description:
+        "Flask is a lightweight WSGI web application framework.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/3/3c/Flask_logo.svg",
+      command: "pip install flask",
+    },
+    {
+      id: 14,
+      name: "Django",
+      url: "https://www.djangoproject.com/",
+      description:
+        "Django is a high-level Python web framework that encourages rapid development and clean, pragmatic design.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg",
+      command: "pip install django",
+    },
+    {
+      id: 15,
+      name: "FastAPI",
+      url: "https://fastapi.tiangolo.com/",
+      description:
+        "FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.6+ based on standard Python type hints.",
+      image:
+        "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png",
+      command: "pip install fastapi",
+    },
+    {
+      id: 16,
+      name: "PyQt",
+      url: "https://riverbankcomputing.com/software/pyqt/intro",
+      description:
+        "PyQt is a set of Python v2 and v3 bindings for The Qt Company's Qt application framework and runs on all platforms supported by Qt including Windows, macOS, Linux, iOS, and Android.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Python_and_Qt.svg/1200px-Python_and_Qt.svg.png",
+      command: "pip install pyqt5",
+    },
   ];
 
   return (
     <>
       <Navbar />
+      <div className="text-center">
+        <button className="my-5 text-xl sm:text-2xl md:text-3xl">
+          <a href="vscode://">
+            Click here to start creating your own AI
+          </a>
+        </button>
+      </div>
+      <div className="mx-5">
+        <button className="my-5 text-md sm:text-lg md:text-xl cursor-default">
+          General OpenSource Python Packages for AI Development
+        </button>
+      </div>
       <div className="flex flex-wrap justify-around my-3">
         {/* load the card components and pass the props */}
         {/* <Card name={aiResources[0].name} url={aiResources[0].url} description={aiResources[0].description} image={aiResources[0].image} /> */}
         {/* load dynamically using map */}
-        
-          {aiResources.map((aiResource) => (
-            <Card
-              key={aiResource.id}
-              name={aiResource.name}
-              url={aiResource.url}
-              desc={aiResource.description}
-              image={aiResource.image}
-            />
-          ))}
+        {pythonPackages.map((pythonPackage) => (
+          <Card
+            key={pythonPackage.id}
+            name={pythonPackage.name}
+            url={pythonPackage.url}
+            desc={pythonPackage.description}
+            image={pythonPackage.image}
+            command={pythonPackage.command}
+          />
+        ))}
       </div>
-      <Footer/>
+
+      <div className="mx-5">
+        <button className="my-5 text-md sm:text-lg md:text-xl cursor-default">
+          Online Platforms for fasttracking your development
+        </button>
+      </div>
+      <div className="flex flex-wrap justify-around my-3">
+        {/* load the card components and pass the props */}
+        {/* <Card name={aiResources[0].name} url={aiResources[0].url} description={aiResources[0].description} image={aiResources[0].image} /> */}
+        {/* load dynamically using map */}
+        {aiResources.map((aiResource) => (
+          <Card
+            key={aiResource.id}
+            name={aiResource.name}
+            url={aiResource.url}
+            desc={aiResource.description}
+            image={aiResource.image}
+          />
+        ))}
+      </div>
+      <Footer />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line react/prop-types
-const Card = ({ name, desc, image, url }) => {
+const Card = ({ name, desc, image, url, command }) => {
     return (
       <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-[#d1d1d1] dark:border-gray-700 m-5
                       w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5
                       h-auto">
         <div className="flex justify-center items-center overflow-hidden">
-          <a href={url}>
+          <a href={url} target="_blank">
             <img
               className="rounded-t-lg p-5 object-contain w-full h-48 sm:h-56 md:h-64 lg:h-72 xl:h-80"
               src={image}
@@ -14,16 +14,18 @@ const Card = ({ name, desc, image, url }) => {
           </a>
         </div>
         <div className="p-5">
-          <a href={url}>
+          <a href={url} target="_blank">
             <h5 className="mb-2 text-xl sm:text-2xl font-bold tracking-tight text-gray-900 dark:text-black">
               {name}
             </h5>
           </a>
+          <p className="mb-2 text-sm sm:text-lg px-2 font-mono tracking-tight bg-[#0b0909] text-[#d1d1d1] ">{command}</p>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-sm sm:text-base">
             {desc}
           </p>
           <a
             href={url}
+            target="_blank"
             className="inline-flex items-center px-3 py-2 text-sm sm:text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Read more
